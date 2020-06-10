2e57ef626334659d2cb726e94f81cf3ef821950b
razamob <razamob@sheridancollege.ca>
2020-06-10 12:46:01 -0400

    Completed Fylo

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const currentTask = process.env.npm_lifecycle_event;
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fse = require('fs-extra');
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+class RunAfterCompile {
+    apply(compiler){
+        compiler.hooks.done.tap('Copy images', function(){
+            fse.copySync('./app/assets/images', './dist/assets/images')
+        })
+    }
+}
+
+let cssConfig = {
+    test: /\.css$/i, 
+    use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+}
+
+let config = {
+    entry: './app/assets/scripts/App.js',
+    plugins: [new HtmlWebpackPlugin({filename: 'index.html', template: './app/index.html'})],
+    module: {
+        rules: [
+            cssConfig
+        ]
+    }
+}
+
+if(currentTask == 'dev'){
+    cssConfig.use.unshift('style-loader')
+    config.output = {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    }
+    config.devServer = {
+        before: function(app, server){
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3005,
+        host: '0.0.0.0'
+    }
+    config.mode = 'development'
+}
+
+if(currentTask == 'build'){
+    config.module.rules.push({
+        test: /\.js$/,
+        exclude: /(node_mudles)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    })
+
+    cssConfig.use.unshift(MiniCSSExtractPlugin.loader)
+    postCSSPlugins.push(require('cssnano'))
+    config.output = {
+        filename: '[name].[chunkhash].js',
+        chunkFilename:'[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+    config.mode = 'production'
+    config.optimization = {
+        splitChunks: {chunks: 'all'}
+    }
+    config.plugins.push(
+    new CleanWebpackPlugin(), 
+    new MiniCSSExtractPlugin({filename: 'styles.[chunkhash].css'}),
+    new RunAfterCompile()
+    )
+}
+
+module.exports = config;
